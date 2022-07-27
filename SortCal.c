1bee7b2d648203fc9761406f327ea819135b27a3
Klad753 <sahil_2101cs69@iitp.ac.in>
2022-07-27 22:56:57 +0530

    Insertion sort added

---
+++ SortCal.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <math.h>
+
+
+
+void insertionSort(int arr[], int n)
+{ int c,t,flag,d;
+    for (c = 1 ; c <= n - 1; c++) {
+    t = arr[c];
+
+    for (d = c - 1 ; d >= 0; d--) {
+      if (arr[d] > t) {
+        arr[d+1] = arr[d];
+        flag = 1;
+      }
+      else
+        break;
+    }
+    if (flag)
+      arr[d+1] = t;
+  }
+}
+
+  
+
+
+void PA(int arr[], int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int n,m, arr[1000];
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+    printf("Enter the array: ");
+    for(int i= 0; i<n; ++i){
+        scanf("%d", &arr[i]);
+    }
+ 
+    printf("1- Insertion Sort \n");
+    printf("2- Selection Sort \n");
+    printf("3- Bubble Sort \n");
+    printf("4- Merge Sort \n");
+    printf("5- Quick Sort \n");
+    printf("Enter Sorting Method: ");
+    scanf("%d", &m);
+
+    switch(m) {
+        case 1:
+        insertionSort(arr, n);
+        PA(arr,n);
+        break;
+       
+
+        default:
+        printf("Please input a valid Sorting Method");
+        break;
+    }
+
+return 0;
+}
